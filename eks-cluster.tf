3449adc5d74327efa2044c9b08ebb10012a76bd5
J-Babi <88941451+J-Babi@users.noreply.github.com>
2022-07-15 13:45:23 -0400

    Update eks-cluster.tf

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -6,7 +6,7 @@
 #
 
 resource "aws_iam_role" "demo-cluster" {
-  name = "eks-dev"
+  name = "eks-Dev"
 
   assume_role_policy = <<POLICY
 {
@@ -35,7 +35,7 @@ resource "aws_iam_role_policy_attachment" "demo-cluster-AmazonEKSServicePolicy" 
 }
 
 resource "aws_security_group" "demo-cluster" {
-  name        = "eks-dev"
+  name        = "eks-Dev"
   description = "Cluster communication with worker nodes"
   vpc_id      = aws_vpc.demo.id
 
@@ -47,7 +47,7 @@ resource "aws_security_group" "demo-cluster" {
   }
 
   tags = {
-    Name = "eks-dev"
+    Name = "eks-Dev"
   }
 }
 
